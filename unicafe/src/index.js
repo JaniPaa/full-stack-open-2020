3fcad013455c0d9046d0f8b5e31c3088568ae670
Jani Paavilainen <jani.paavilainen@netti.fi>
2020-09-18 16:30:21 +0300

    Tehtävät 1.8-1.10

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -1,6 +1,48 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Button = (props) => {
+  return (
+    <div>
+      <button onClick={props.action}>
+        {props.name}
+      </button>
+    </div>
+  )
+}
+
+const StatisticLine = (props) => {
+  return(
+    <div>
+      <p>{props.text} {props.value}</p>
+    </div>
+  )
+}
+
+const Statistics = (props) => {
+
+  if (props.stats.all === 0){
+    return (
+      <div>
+        <h2>Statistics</h2>
+        <h3>No feedback given</h3>
+      </div>
+    )
+  } else{
+    return (
+      <div>
+      <h2>Statistics</h2>
+      <StatisticLine text="Good" value={props.stats.good}/>
+      <StatisticLine text="Neutral" value={props.stats.neutral}/>
+      <StatisticLine text="Bad" value={props.stats.bad}/>
+      <StatisticLine text="All" value={props.stats.all}/>
+      <StatisticLine text="Average" value={props.stats.average}/>
+      <StatisticLine text="Positive" value={props.stats.positive}/>
+      </div>
+    )
+  } 
+}
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
@@ -24,25 +66,22 @@ const App = () => {
     return (good / all) * 100
   }
 
+  const stats = {
+    good: good,
+    neutral: neutral,
+    bad: bad,
+    all: allFeedback(good, neutral, bad),
+    average: getAverage(good, bad, allFeedback(good, neutral, bad)),
+    positive: getPositivePercentage(good, allFeedback(good, neutral, bad))
+  }
+
   return (
     <div>
       <h2>Give feedback</h2>
-      <button onClick={increaseGood}>
-        Good
-      </button>
-      <button onClick={increaseNeutral}>
-        Neutral
-      </button>
-      <button onClick={increaseBad}>
-        Bad
-      </button>
-      <h2>Statistics</h2>
-      <p>Good {good}</p>
-      <p>Neutral {neutral}</p>
-      <p>Bad {bad}</p>
-      <p>All {allFeedback(good, neutral, bad)}</p>
-      <p>Average {getAverage(good, bad, allFeedback(good, neutral, bad))}</p>
-      <p>Positive {getPositivePercentage(good, allFeedback(good,neutral, bad))}</p>
+      <Button name="Good" action={increaseGood}/>
+      <Button name="Neutral" action={increaseNeutral}/>
+      <Button name="Bad" action={increaseBad}/>    
+      <Statistics stats={stats}/>
     </div>
   )
 }
